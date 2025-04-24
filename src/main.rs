ac63726b99343394147d44eca0e9dddf9eb8d596
Abigail Lee <abbylee@bu.edu>
2025-04-24 15:15:04 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+
+    println!("This is a test of git");
+
+    println!("Now I'm making more edits to see what happens");
+}
+
+
+//todo: read_csv
+//create graph
+//operations from there
+
+
+
+//FOR OFFICE HOURS
+//git? project idea? how to do visualization?

11ef8138b83a9e0328775feae57840d0ad3c7562
Abigail Lee <abbylee@bu.edu>
2025-04-30 20:46:50 -0400

    edited read_csv, added Student struct

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,31 @@
 use std::fs::File;   
 use std::io::prelude::*;
-use std::collections::HashMap;
+mod tree;
 
-pub type Vertex = usize;
-type ListOfEdges = Vec<(Vertex,Vertex)>;
-type AdjacencyLists = Vec<Vec<Vertex>>;
-type ListOfWeights = Vec<usize>;
-pub struct Graph {
-    pub n: usize,  
-    pub outedges: AdjacencyLists,
-    pub total_edges: ListOfEdges,
-    pub weights: ListOfWeights,
+pub struct Student {
+    features: Vec<f64>,
+    label: f64,
 }
 
-fn read_csv_to_hashmap(path: &str) -> HashMap<usize, Vec<f64>> {
-    let mut answer = HashMap::new();              
+pub fn read_csv(path: &str) -> (Vec<String>, Vec<Student>) {        //should return a vector of Students
+    let mut answer = Vec::new();     
+    let mut feature_names: Vec<String> = Vec::new();         
     let file = File::open(path).expect("Could not open file");         
     let buf_reader = std::io::BufReader::new(file).lines();   
 
     let mut first_row = true;
 
-    for (index, line) in buf_reader.enumerate() {
+    for line in buf_reader.into_iter() {
         let line_str = line.expect("Error reading");
 
         if first_row {
+            feature_names = line_str
+                                                .split(";").map(|s| s.to_string()).collect();
             first_row = false;
             continue;  
         }
     
-        let features: Vec<f64> = line_str
+        let full_row: Vec<f64> = line_str
                                                 .split(";")
                                                 .map(|s| {
                                                     match s { 
@@ -40,23 +37,40 @@ fn read_csv_to_hashmap(path: &str) -> HashMap<usize, Vec<f64>> {
                                                 }
                                                     )
                                                 .collect();
+        
+        let label = full_row[full_row.len()-1];
+        let features = full_row[0..full_row.len()-1].to_vec();
 
-        answer.insert(index, features);
+        let student = Student {features, label};
+
+        answer.push(student);
             }
-    answer
+    (feature_names, answer)
     }
 
-
 fn main() {
-    read_csv_to_hashmap("college_data.csv");
+
 }
 
-
-//todo: read_csv
 //create graph
 //operations from there
 
 
+//k nearest neighbors to see if there are clusters within the features 
+//draw clusters in two dimensions using dimensionality reduction techniques; use ndarray 
+//use PCA to reduce dimensionality 
+//use a cargo crate for different clusters, everyone gets assigned to a cluster 
+// measure the distance of every point to its centroid in its cluster
+// the more clusters, the closer every point is going to be to its centroid 
+// use k means cluster plot in plotly 
+// run PCA to project points into two dimensions, plot that as a scatter plot BEFORE K MEANS, get a sense of if there is a random cloud
+// if random cloud, k means might not work that well 
+// if clusters, k means will work well 
+// it's ok if you don't get results, not a DS project, just run PCA and k means and it'll be fine 
 
-//FOR OFFICE HOURS
-//git? project idea? how to do visualization?
+
+// run a decision tree algorithm to predict k means; k means tells you similarity, decision tree predicts 
+// K MEANS OR DECISION TREE
+
+//try decision tree first 
+
